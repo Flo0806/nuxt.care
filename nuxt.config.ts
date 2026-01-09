db97f2cb12359f113654d2cd7f794bbe241344d8
Florian Heuberger <fh@flogersoft.de>
2026-01-09 18:09:06 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/a11y',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
